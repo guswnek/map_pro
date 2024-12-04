0d696ee653d5c532fbf86a0b5bad617c2f54c216
guswnek1002@naver.com <guswnek1002@naver.com>
2024-12-04 14:06:10 +0900

    파일 설정

---
+++ frontend/src/components/MainMap.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="main-map">
+
+  </div>
+</template>
+
+<script>
+import {onMounted} from "vue";
+import OlLayerTile from 'ol/layer/Tile.js';
+import OlView from 'ol/View.js';
+import OlMap from 'ol/Map.js';
+import OSM from 'ol/source/OSM';
+import {fromLonLat} from 'ol/proj.js'
+
+let olMap = undefined;
+
+onMounted(() => {
+  olMap = new olMap({
+    target: this.$refs.map,
+    layers: [
+      new OlLayerTile({
+        source: new OSM()
+      })
+    ],
+    view : new OlView({
+      center: fromLonLat([127.1388684, 37.4449168]), // 경기도 성남
+      zoom: 10
+    })
+  })
+})
+</script>
+
+<style scoped>
+  .main-map{
+    width: 100%;
+    height: 100%;
+  }
+</style>
