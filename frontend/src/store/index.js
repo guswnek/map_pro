0841ef9a65190ce2b012aaafcf2aca636d970069
guswnek1002@naver.com <guswnek1002@naver.com>
2025-01-21 17:32:51 +0900

    7

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -18,6 +18,7 @@ export default createStore({
             console.log("3");
             console.log("4");
             console.log("5");
+            console.log("7");
         }
     },
     actions: {
